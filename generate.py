917353a047d7fe248a5502a933e592af549aae58
Joshua Reynolds <jreynolds@electrichand.com>
2023-06-01 20:15:28 +0200

    feat: add ability to generate html/pdf using asciidoc

---
+++ generate.py
@@ -0,0 +1,35 @@
+import sys
+
+import anyio
+import dagger
+
+ASCIIDOCTOR_IMAGE="asciidoctor/docker-asciidoctor"
+SRC_DIR="./src"
+BUILD_DIR="./build"
+RESUME="README.adoc"
+
+async def docs():
+    config = dagger.Config(log_output=sys.stderr)
+
+    async with dagger.Connection(config) as client:
+
+        project = client.host().directory(".")
+
+        build_dir = (
+            client.container()
+            .from_(ASCIIDOCTOR_IMAGE)
+            .with_directory(SRC_DIR, project)
+            .with_workdir(SRC_DIR)
+            .with_exec(["mkdir", "-p", BUILD_DIR])
+            .with_exec(["asciidoctor", "-r", "asciidoctor-pdf", "-b", "pdf", "-r", "asciidoctor-diagram", "-D", BUILD_DIR, RESUME])
+            .directory(BUILD_DIR)
+        )
+
+
+        await build_dir.export(BUILD_DIR)
+
+        e = await build_dir.entries()
+
+        print(e)
+
+anyio.run(docs)
