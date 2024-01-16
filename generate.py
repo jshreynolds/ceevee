b42462ef725bb204cf1c83b1bc769e589fd71bf9
Joshua Reynolds <jreynolds@electrichand.com>
2024-01-16 10:33:39 +0100

    fix: update the dagger.io script to rename the ceevee appropriately

--- generate.py
+++ generate.py
@@ -3,16 +3,16 @@ import sys
 import anyio
 import dagger
 
-ASCIIDOCTOR_IMAGE="asciidoctor/docker-asciidoctor"
-SRC_DIR="./src"
-BUILD_DIR="./build"
-RESUME="README.adoc"
+ASCIIDOCTOR_IMAGE = "asciidoctor/docker-asciidoctor"
+SRC_DIR = "./src"
+BUILD_DIR = "./build"
+RESUME = "README.adoc"
+
 
 async def docs():
     config = dagger.Config(log_output=sys.stderr)
 
     async with dagger.Connection(config) as client:
-
         project = client.host().directory(".")
 
         build_dir = (
@@ -21,15 +21,30 @@ async def docs():
             .with_directory(SRC_DIR, project)
             .with_workdir(SRC_DIR)
             .with_exec(["mkdir", "-p", BUILD_DIR])
-            .with_exec(["asciidoctor", "-r", "asciidoctor-pdf", "-b", "pdf", "-r", "asciidoctor-diagram", "-D", BUILD_DIR, RESUME])
+            .with_exec(
+                [
+                    "asciidoctor",
+                    "-r",
+                    "asciidoctor-pdf",
+                    "-b",
+                    "pdf",
+                    "-r",
+                    "asciidoctor-diagram",
+                    "-D",
+                    BUILD_DIR,
+                    "-o",
+                    "joshua.reynolds.resume.pdf",
+                    RESUME,
+                ]
+            )
             .directory(BUILD_DIR)
         )
-
 
         await build_dir.export(BUILD_DIR)
 
         e = await build_dir.entries()
 
         print(e)
+
 
 anyio.run(docs)
