762f5d92c751656c7bb2c2ee3177e8206bc547fb
Joshua Reynolds <jreynolds@electrichand.com>
2024-02-01 16:50:22 +0100

    fix: correct typo: Help -> Helm

--- publish_version.sh
+++ publish_version.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 poetry version patch && \
+poetry install && \
 git add pyproject.toml && \
 git commit -m "fix: patching version for release" && \
 git tag $(poetry version --short) && \
